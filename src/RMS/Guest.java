010399ca969c2c425ac900b6d777d3dfaff1820e
Adrien <vonfeld@gmail.com>
2021-10-21 16:33:32 +0300

    GuestFields
    Added fields in Guest class.
    Changed Project Name to RMS

--- src/RMS/Guest.java
+++ src/RMS/Guest.java
@@ -1,2 +1,9 @@
-package RMS;public class Guest {
+package RMS;
+
+public class Guest {
+    private String lastName;
+    private String firstName;
+    private String email;
+    private String phoneNumber;
+
 }
